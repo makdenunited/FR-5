ccbd3f5009336548b284c9fc467593e20a0b2b8b
Aleksandrova Oksana <aleksandrovao767@gmail.com>
2023-06-24 18:14:16 +0300

    first commit

---
+++ oks767/main.js
@@ -0,0 +1,26 @@
+function calc(a, b, operation) {
+  switch (operation) {
+    case 'add':
+      c = a + b;
+
+      break;
+    case 'multi':
+      c = a * b;
+
+      break;
+    case 'subtract':
+      c = a - b;
+
+      break;
+  }
+  if (Number.isNaN(c) === true) {
+    alert('Пожалуйста введите числа');
+  } else {
+    alert(c);
+  }
+}
+let a = +prompt('Введите число', '');
+let b = +prompt('Введите второе число', '');
+let operation = prompt('Введите действие: add, multi, substract', '');
+
+calc(a, b, operation);
