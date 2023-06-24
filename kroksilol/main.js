57a8af3f097c974814eea8be2f6dd13b37a21f59
kroksilol <kroks1337@gmail.com>
2023-06-24 16:02:20 +0300

    kroksilol/practise-13

---
+++ kroksilol/main.js
@@ -0,0 +1,17 @@
+function calc(operation, a, b){
+    switch (operation, a, b) {
+      case (operation === 'add') && a && b:
+         console.log(a + b)
+          break;
+      case (operation === 'mult') && a && b:
+          console.log(a * b)
+          break;
+      case (operation === 'sub') && a && b:
+          console.log(a - b)
+         break;
+      default:
+         break;
+    }
+  }
+
+  calc('mult', 10, 15);
