f79fdd2dc5f8cc22a29d7cb01752fb3143b9fc45
SamGQ <kombokesfet@gmail.com>
2023-06-25 01:56:51 +0300

    first project

---
+++ main.js
@@ -0,0 +1,14 @@
+function checkAge(age) {
+  switch (age) {
+    case age < 18:
+      console.log("You are not allowed.");
+      break;
+    case age >= 18:
+      console.log("You are welcome!");
+      break;
+    default:
+      console.log("Good.");
+  }
+}
+
+checkAge(18);
