2a4e1a6de9d44dacfc54266b2e8dcf10b9a8fb1f
unknown <D:\The Bat!>
2023-06-24 17:15:12 +0300

    Я не понимаю, что я делаю тт

---
+++ ksuanja/main.js
@@ -0,0 +1,13 @@
+function calc(a, b, operation) {
+    switch (operation) {
+        case 'add':
+            return(a + b);
+            break;
+        case 'multi':
+            return(a * b);
+            break;
+        case 'subtract':
+            return(a - b);
+            break;
+    };
+};
