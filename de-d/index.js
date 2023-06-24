b0a73eb466bc44641abf838cad878ee9644bfa19
de-d <etzheded@gmail.com>
2023-06-24 15:55:04 +0300

    small-corrections

--- de-d/index.js
+++ de-d/index.js
@@ -10,10 +10,13 @@ function calculate(operation, a, b) {
     switch (operation) {
         case 'add':
             return a + b;
+            break;
         case 'multi':
             return a * b;
+            break;
         case 'subtract':
             return a - b;
+            break;
     }
 }
 let result = calculate('multi', 2, 2);
