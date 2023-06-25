0d7b424bd761da8f8f86ac27d3c56f320da9fb06
EgorK <egorkinaevskij@gmail.com>
2023-06-25 13:49:17 +0300

    1version

---
+++ Egor/main.js
@@ -0,0 +1,20 @@
+let a=1
+let b=2
+let c=3
+
+switch (a,b)
+ {
+        case a,b:
+                console.log (a+b);
+        
+                break;
+        case a,b:
+                console.log (a*b);
+        case c,b:
+                console.log (c%b);
+                
+                break;
+
+        
+         
+}
