ead22bac88dad382faee56c60a99493c9b98aded
EgorK <egorkinaevskij@gmail.com>
2023-06-25 14:25:45 +0300

    2version

--- Egor/main.js
+++ Egor/main.js
@@ -1,20 +1,25 @@
-let a=1
-let b=2
-let c=3
+let a = 1
+let b = 2
+let c = 3
+function calc(a, b, operation) {
+        switch (operation) {
 
-switch (a,b)
- {
-        case a,b:
-                console.log (a+b);
-        
-                break;
-        case a,b:
-                console.log (a*b);
-        case c,b:
-                console.log (c%b);
-                
-                break;
+                case  'add' :
+                        console.log(a + b);
 
-        
-         
+                        break;
+                case 'multi' :
+                        console.log(a * b);
+                        break;
+                case 'subtract':
+                        console.log(c % b);
+
+                        break;
+
+
+
+        }
 }
+calc(1,2,'add')
+calc(1,2,'multi')
+calc(3,2,'subtract')  
