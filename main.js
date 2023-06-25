792b56bf61ea7f4346c3a9915a1b087b1e2ec772
RazDva222 <137118650+RazDva222@users.noreply.github.com>
2023-06-26 02:13:32 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function  calc(operatrion,a,b){
+    switch(operatrion){
+      case 'add':
+        return a + b;
+      case 'multi':
+        return a * b;
+      case 'subtract':
+        return a-b;
+    }
+  }
