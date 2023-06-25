f64f4e18d24eb415cfeb6a303fa48ceb2a92aeac
Kseniya Mazaeva <kitsune@MacBook-Pro-Kseniya.local>
2023-06-25 19:22:33 +0300

    изминения

---
+++ kits/main.js
@@ -0,0 +1,20 @@
+function calc(operation, a, b) {
+    switch (operation) {
+      case 'add':
+        a + b;
+        console.log(a + b);
+        break;
+      case 'multi':
+        a * b;
+        console.log(a * b);
+        break;
+      case 'subtract':
+        a - b;
+        console.log(a - b);
+        break;
+    }
+  }
+  
+  calc('add', 1, 2);
+  calc('multi', 1, 2);
+  calc('subtract', 3, 2);
