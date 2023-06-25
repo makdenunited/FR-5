2bca29c65bc97be4449d52f21e52c2e4414c5b78
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-06-26 00:46:58 +0300

    fix: change multi operation and delete checkAge function

--- MaxLisyanskiy/main.js
+++ MaxLisyanskiy/main.js
@@ -1,25 +1,17 @@
-function checkAge(age) {
-    return age < 18 
-        ? console.log("you are not allowed") 
-        : console.log("you are welcome!")
-}
-checkAge(21)
-
-// **************** //
-
 function сalc(operation, a, b) {
     switch(operation) {
         case "add":
-            return console.log(a + b)
+            return console.log(a + b);
         case "multi":
-            return console.log(a > b ? a : b)
+            return console.log(a * b);
         case "subtract":
-            return console.log(a - b)
+            return console.log(a - b);
         default:
-            return console.error("Operation is not defined")
+            return console.error("Operation is not defined");
     }
 }
-сalc("add", 1, 2)
-сalc("multi", 1, 2)
-сalc("subtract", 3, 2)
-сalc("error", 3, 2)
+
+сalc("add", 1, 2);
+сalc("multi", 1, 2);
+сalc("subtract", 3, 2);
+сalc("error", 3, 2);
