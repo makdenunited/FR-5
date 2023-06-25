666a5d7dc97a895fdc901fced02e91df1ad0c10c
Stasual <ryutin.95@mail.ru>
2023-06-25 22:45:35 +0300

    change solution

--- stasryutin/index.js
+++ stasryutin/index.js
@@ -1,17 +1,20 @@
-let a = 1;
-let b = '2';
-let operation = 'add';
-
-switch (operation) {
-	case 'add':
-		alert(+a + +b);
-		break;
-	case 'multi':
-		alert(+a * +b);
-		break;
-	case 'subtract':
-		alert(+a - +b);
-		break;
-	default:
-		alert("Нет таких значений");
+function сalc(operation, a, b) {
+	switch (operation) {
+		case 'add':
+			return +a + +b;
+			break;
+		case 'multi':
+			return +a * +b;
+			break;
+		case 'subtract':
+			return +a - +b;
+			break;
+		default:
+			return ("Нет таких значений");
+	}
 };
+
+console.log(сalc('multi', 27, '27'));
+console.log(сalc('add', '23', 23));
+console.log(сalc('subtract', 7, true));
+console.log(сalc('none', 1, 1));
