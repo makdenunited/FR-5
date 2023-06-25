f552f8a7491abd079c7b43bbffff76288176617f
Tamper <tamper.powder@gmail.com>
2023-06-25 16:42:45 +0300

    change js

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 function calc(operation,a,b){
     switch(operation){
         case 'add':
-            return a+b;
-            break;
+            return a + b;
         case 'multi':
-            return a*b;
-            break;
+            return a * b;
         case 'div':
-            return a/b;
-            break;
+            return a / b;
         case 'sub':
-            return a-b;
-            break;
+            return a - b;
         default:
             return "I dont know thise operation";
     }
 }
+console.log(calc('add' ,1,2));
+console.log(calc('multi' ,1,2));
+console.log(calc('div' ,1,2));
+console.log(calc('sub' ,1,2));
 console.log(calc('' ,1,2));
