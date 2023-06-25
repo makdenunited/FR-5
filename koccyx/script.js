4299b12b30d5d99ef0a79683e0e29c0b0ccd69d7
koccyx <rings@mail.ru>
2023-06-25 18:14:57 +0300

    switch case

---
+++ koccyx/script.js
@@ -0,0 +1,18 @@
+function calc1(operation, a, b) {
+    switch(operation) {
+        case 'add':
+        return a + b;
+        case 'multi':
+        return a * b;
+        case 'substract':
+        return a - b;
+        default: 
+        return 'wrong operation';
+    }
+}
+
+  
+console.log(calc1("add", 30, 10));
+console.log(calc1("multi", 2, 4));
+console.log(calc1("substract", 30, 21));
+console.log(calc1("do smth", 201, 10));
