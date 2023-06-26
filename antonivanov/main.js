8b8d90944e0e15d2da190f6c089b3da5b3ba0c5f
HUAWEI <anton.ivanov.qa@gmail.com>
2023-06-26 18:32:13 +0300

    изменил имя переменной для счётчика

--- antonivanov/main.js
+++ antonivanov/main.js
@@ -23,8 +23,8 @@ for (let j = 1; j < 20; j++) {
 
 // Пропускаем итерацию, когда i равно 2 c помощью continue
 
-for (var i = 0; i < 5; i++) {
-  if (i === 2) {
+for (var l = 0; i < 5; i++) {
+  if (l === 2) {
     continue;
   }
   console.log(i);
