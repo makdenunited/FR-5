35eeac48d74c90bb7c0a0aae89b9d4697e22b156
HoldThisWoe <19102it@gmail.com>
2023-06-26 22:28:34 +0400

    Правки по комменатриям

--- HoldThisWoe/main.js
+++ HoldThisWoe/main.js
@@ -1,24 +1,14 @@
-let a = 4;
-let b = 2;
-let operation = 'substract';
-let result;
-
 function calc(a=0, b=0, operation){
     switch(operation){
         case 'add':
             return a + b;
-            break;
-        case 'substract':
+        case 'subtract':
             return a - b;
-            break;
         case 'multiply':
             return a * b;
-            break;
         default:
             return 'No operation is selected';
-            break;
     }
 }
 
-result = calc(a, b, operation);
-console.log(result);
+console.log(calc(2, 2, 'add'));
