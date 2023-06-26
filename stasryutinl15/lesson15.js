47e057c802843f07ca21410f3730939415d6fc2c
Stasual <ryutin.95@mail.ru>
2023-06-26 19:49:04 +0300

    добавил решение к 1 заданию

--- stasryutinl15/lesson15.js
+++ stasryutinl15/lesson15.js
@@ -1,7 +1,23 @@
+// ЗАДАНИЕ №1
+
+let i = 1;
+while (i < 20) {
+	console.log(i)
+	i = i + 1;
+}
+
+let i = 1;
+
+do {
+	console.log(i);
+	i = i + 1;
+} while (i < 20);
+
+// ЗАДАНИЕ №2
+
 for (i = 1; i < 20; i++) {
 	console.log(i);
 }
-
 
 let i = 1;
 
