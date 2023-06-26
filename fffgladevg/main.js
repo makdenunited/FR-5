2f5c9cec17d848534ff1be598ae8168ce74761f4
fffgladevg <mrmrmr1487@gmail.com>
2023-06-27 00:38:04 +0300

    refactor:main

--- fffgladevg/main.js
+++ fffgladevg/main.js
@@ -1,20 +1,3 @@
-// console.log('example');
-// console.log(typeof('string'));
-
-// function someFunc() {
-//     console.log('function start here')
-// }
-//someFunc();
-// function checkAge(age) { // объявляем функцию sayHi с параметром name
-//     //const funcAge = age;
-//     if (age < 18) {
-//         console.log('u are not allowed');
-//     } else {
-//             console.log('u are welcome');
-//         }
-//   }
-//   checkAge(19);
-
 function calc(operation, a, b) {
     switch (operation){
         case 'add':
