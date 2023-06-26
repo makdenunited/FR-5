15f5fa0cbc9b2b8512f4418946557c8ba87a30a5
Alex Sparrow <alexsparrow5973@gmail.com>
2023-06-26 18:52:09 +0300

    fixed calc.js after code review

--- AlexSparrow/calc.js
+++ AlexSparrow/calc.js
@@ -3,23 +3,22 @@ function calc(operation, a, b) {
     b = Number(b);
     switch (operation) {
       case 'add':
-        result = a + b;
+        return a + b;
         break;
       case 'multi':
-        result = a * b;
+        return a * b;
         break;
       case 'subtract':
-        result = a - b;
+        return a - b;
         break;
       case 'division':
-        result = (a / b === Infinity) ? 'На ноль делить нельзя' : a / b;
+        return (a / b === Infinity) ? 'На ноль делить нельзя' : a / b;
         break;
     }
-    return console.log(`${result}`);
 }
 
-calc('add', 1, 2);
-calc('multi', 1, 2);
-calc('subtract', 3, 2);
-calc('division', 1, 2);
-calc('division', 1, 0);
+console.log(calc('add', 1, 2));
+console.log(calc('multi', 1, 2));
+console.log(calc('subtract', 3, 2));
+console.log(calc('division', 1, 2));
+console.log(calc('division', 1, 0));
