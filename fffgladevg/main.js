d2a61154153876a8ea2a26f56da34a335525407f
fffgladevg <mrmrmr1487@gmail.com>
2023-06-27 00:53:54 +0300

    refactor:main add const

--- fffgladevg/main.js
+++ fffgladevg/main.js
@@ -1,16 +1,16 @@
+const add = "add";
+const sub = "substract";
+const multi = "multi"
 function calc(operation, a, b) {
     switch (operation){
-        case 'add':
+        case add:
             return a + b;
-            break;
-        case 'multi':
+        case multi:
             return a * b;
-            break;
-        case 'substract':
+        case sub:
             return a - b;
-            break;
     }
 }
-console.log(calc('add', 1, 5));
-console.log(calc('substract', '1', '2'));
-console.log(calc('multi', 2*2, 5));
+console.log(calc(add, 1, 5));
+console.log(calc(sub, '1', '2'));
+console.log(calc(multi, 2*2, 5));
