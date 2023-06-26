202a8ca855c82d4b68d747dec8f88dcaae22540b
ForeverBlade <tkp_r@mail.ru>
2023-06-26 21:23:08 +0300

    15 ЦИКЛЫ

---
+++ main15.js
@@ -0,0 +1,18 @@
+let i = 1;
+while (i < 20) {
+  console.log(i)
+  i = i + 1;
+ }
+
+let k = 1;
+do {
+  console.log(k);
+	k = k + 1;
+} 
+while (k < 20);
+
+let j = 1;
+for (; j < 20;) {
+    console.log(j);
+      j = j + 1;
+  }
