0aa2c096d519011a98dcc46ee5f258ead61c5802
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-06-26 10:04:11 +0300

    init: start practice 15

---
+++ MaxLisyanskiy/main.js
@@ -0,0 +1,20 @@
+/* While */
+function handleWhile() {
+    let i = 1;
+
+    while (i < 20) {
+        console.log(i);
+        i++
+    }
+}
+handleWhile();
+
+function handleDoWhile() {
+    let i = 1;
+
+    do {
+        console.log(i);
+        i++;
+    } while (i < 20);
+}
+handleDoWhile()
