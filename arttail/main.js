72d289e5de7bef80aed572e2a302e65c546ec74a
Artem K <art_tail@mail.ru>
2023-06-26 18:52:34 +0300

    Loops: while and for

---
+++ arttail/main.js
@@ -0,0 +1,18 @@
+//while
+let i = 1;
+while (i < 20) {
+    console.log(i);
+    i++;
+}
+
+// do ... while
+let j = 1;
+do {
+    console.log(j);
+    j++;
+} while (j < 20);
+
+// for
+for (let k = 1; k < 20; k++) {
+    console.log(k);
+}
