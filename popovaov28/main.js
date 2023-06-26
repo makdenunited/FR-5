5e6167eccac9afabfb9184f24f609db6c8f6d28a
vxrossa <ibasalaev@icloud.com>
2023-06-26 19:02:42 +0600

    practice 15

---
+++ popovaov28/main.js
@@ -0,0 +1,26 @@
+let i = 1;
+while (i < 20) {
+  console.log(i);
+  i = i + 1;
+}
+
+let k = 1;
+do {
+  console.log(k);
+  k = k + 1;
+} while (k < 20);
+
+for (let l = 1; l < 20; l++) {
+  console.log(l);
+}
+
+let j = 1;
+for (; j < 20; j++) {
+  console.log(j);
+}
+
+let m = 1;
+for (; m < 20; ) {
+  console.log(m);
+  m = m + 1;
+}
