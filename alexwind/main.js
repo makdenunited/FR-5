3315fc526f86bc6d9b9fa312ba875499c67fbdc2
Reddy1989 <online1989@mail.ru>
2023-06-26 13:15:56 +0300

    #15 cycles While, doWhile, For

---
+++ alexwind/main.js
@@ -0,0 +1,31 @@
+// while
+console.log('------- While -------')
+
+let counterForWhile = 0;
+
+while (counterForWhile < 19) {        
+    counterForWhile++
+    console.log(counterForWhile);     
+}
+
+// do while
+
+console.log('------- DoWhile -------')
+
+let counterForDoWhile = 1;
+
+do {
+    console.log(counterForDoWhile);
+    counterForDoWhile++;
+    } while(counterForDoWhile < 20);
+
+// for
+
+console.log('------- for -------')
+
+let counterForCycleFor
+
+for (let counterForCycleFor = 1; counterForCycleFor < 20; counterForCycleFor++) {
+        console.log(counterForCycleFor);
+}
+
