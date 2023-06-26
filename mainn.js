4716627f16610948bfe9b8ff0004bef6a9d74ddf
Loonamia <mehroma06@gmail.com>
2023-06-27 01:14:52 +0500

    Циклы

---
+++ mainn.js
@@ -0,0 +1,19 @@
+/*
+let i = 1;
+while (i < 20) {
+    console.log(i)
+    i = i + 1;
+}
+*/
+
+/*
+let i = 1;
+do {
+    console.log(i);
+    i = i + 1;
+} while (i < 20);
+*/
+
+for (i = 1; i < 20; i++) {
+    console.log(i);
+}
