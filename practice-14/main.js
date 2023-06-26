0f8d574aded96e6a300a4fe6d573a8a2dfc30e95
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-06-26 11:37:44 +0400

    first commit

---
+++ practice-14/main.js
@@ -0,0 +1,18 @@
+// Цикл с помощью while
+let i = 1;
+while (i < 19) {
+  console.log(i);
+  i = i + 1;
+}
+
+// Цикл с помощью do/while
+let j = 1;
+do {
+  console.log(j);
+  j = j + 1;
+} while (j < 19);
+
+// С помощью цикла for
+for (let i = 1; i <= 19; i++) {
+  console.log(i);
+}
