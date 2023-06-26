135e44fc6411fd1af32e240756941b12e5f4e602
firejak1887 <firejak1887@bk.ru>
2023-06-26 18:01:48 +0300

    1

---
+++ Test14/test15.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 12; i++) {
+    if (i % 4 == 0) continue;
+    console.log(i);
+  }
+
+let i = 1;
+
+do {
+  console.log(i);
+	i = i + 1;
+} while (i < 20);
+
+
+for (let i = 0; i < 20; i++) {
+    console.log(i);
+}
+
+
