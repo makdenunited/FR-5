da7f9cc0feb94fed0f82618971968850c63499b3
bulatvk <bulatvk@gmail.com>
2023-06-28 01:10:25 +0500

    minor change

--- main.js
+++ main.js
@@ -3,15 +3,6 @@ while (j < 5) {
 	console.log(j)
 	j = j + 1;
 }
-// переписать на for
-//пинг
-for (;j > 1; j--) {
-	console.log(j);
-}
-//понг
-for (;j < 6; j++) {
-	console.log(j);
-}
 
 let k = 5;
 do {
@@ -19,23 +10,37 @@ do {
 	k = k + 1;
 } while (k < 3);
 
+for (;j > 1; j--) {
+	console.log(j);
+}
+
+for (;j < 6; j++) {
+	console.log(j);
+}
+
+function chetNechet(a) {
 let chet = 0;
 let nechet = 0;
 
-for (let l = 1; l <= 8; l++) {
+for (let l = 1; l <= a; l++) {
 	if (l % 2 === 0) {
 		chet++;
 	continue;
-}
+	}
 		nechet++;
-}
+	}
 console.log(`Чётных ${chet}`)
 console.log(`Нечётных ${nechet}`)
-
-for (let i = 25; i > 0; i--) {
-	let on = i % 10;
-	let sp = i > 10 && i < 15;
-	let pf = on > 1 && on < 5 && !sp;
-
-	console.log(`Остал${on > 1 || on === 0 || sp ? 'О' : 'А'}сь ${i} попыт${pf ? 'КИ' : on !== 1 || sp ? 'ОК' : 'КА'}.`);
 }
+chetNechet(301)
+
+function sklonenieScheta(a) {
+	for (let i = a; i > 0; i--) {
+		let on = i % 10;
+		let sp = i > 10 && i < 15;
+		let pf = on > 1 && on < 5 && !sp;
+
+		console.log(`Остал${on > 1 || on === 0 || sp ? 'О' : 'А'}сь ${i} попыт${pf ? 'КИ' : on !== 1 || sp ? 'ОК' : 'КА'}.`);
+	}
+}
+sklonenieScheta(114)
