181c9a67ebfbd04336711acdb2ef8d29833f51d9
vitmord <vitmord@gmail.com>
2023-06-27 18:39:18 +0300

    init: task 15

--- vitmord/main.js
+++ vitmord/main.js
@@ -1,12 +1,27 @@
-function calc(operation, a, b) {
-    switch (operation) {
-        case 'add':
-            return a + b
-        case 'multi':
-            return a * b
-        case 'subtract':
-            return a - b
-    }
+console.log('1) while loop')
+let i = 1
+
+while (i <= 3) {
+    console.log(i++)
 }
 
-console.log(calc('multi', 3, 2))
+
+console.log('2) do while loop')
+let j = 3
+
+do {
+    console.log(j--);
+} while (j > 0)
+
+console.log('3) for loop');
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log('4) for loop with continue')
+
+for (let i = 1; i <= 33; i++) {
+    if (i % 3 !== 0) continue
+    console.log(i);
+}
