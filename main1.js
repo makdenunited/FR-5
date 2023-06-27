5c0bb6d3f431339ae3ad561a27e2039319154848
Roman <120248964+host002@users.noreply.github.com>
2023-06-27 08:09:37 +0300

    host/practice-16

---
+++ main1.js
@@ -0,0 +1,11 @@
+const phoneContact = {
+  Piter: 7921333445,
+  'Santa Claus': 7921557733,
+
+};
+console.log(phoneContact);
+ phoneContact['Piter'] = 7921333445;
+console.log(phoneContact);
+ phoneContact['Santa Claus'] = 7921557733;
+console.log(phoneContact);
+delete phoneContact['Piter'];
