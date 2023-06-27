a920936c559a00b9696960c365dc039e82921ce1
Leonid <leonidhaidukov@yandex.ru>
2023-06-27 08:21:36 +0700

    New Function for count

--- leonixx practise 15/main.js
+++ leonixx practise 15/main.js
@@ -1,22 +1,36 @@
 // Вывести в консоль числа от 1 до 19 с помощью цикла
 // Сделать то же самое но с проверкой условия после тела цикла (do ... while)
 
-let i = 1; 
-while(i <= 19) {
-    console.log(i)
-    i++
-}
+// let i = 1; 
+// while(i <= 19) {
+//     console.log(i)
+//     i++
+// }
 
-let k = 0;
-do {
-    k = k + 1
-    console.log(k)
-} while(k <= 18)
+// let k = 0;
+// do {
+//     k = k + 1
+//     console.log(k)
+// } while(k <= 18)
 
-for(let i = 1; i <=19; i++) {
-    console.log(i)
-}
+// for(let i = 1; i <=19; i++) {
+//     console.log(i)
+// }
 
-for(let i = 1; i <=19; ++i) {
-    console.log(i)
+// for(let i = 1; i <=19; ++i) {
+//     console.log(i)
+// }
+function CountNumber(k) {
+    let twoQual = 0;
+    let oneQual = 0;
+    for(let i = 1; i <= k; i++) {
+    if(i % 2 === 0) {
+        twoQual = twoQual + 1
+    }
+    else {
+        oneQual = oneQual + 1
+    }
+    }
+    return `two number - ${twoQual}; one number - ${oneQual}`
 }
+console.log(CountNumber(15))
