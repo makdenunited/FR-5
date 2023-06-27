8fbf205b07c304bf215e0306556731d0c4aed256
vway1 <one1way55@gmail.com>
2023-06-28 01:29:57 +0300

    циклы

---
+++ vway1/15/main.js
@@ -0,0 +1,16 @@
+let i = 1;
+while (i <= 19) {
+    console.log(i++);
+}
+
+
+let i = 1;
+do {
+    console.log(i++);
+} while (i <= 19);
+
+
+for (let i = 1; i <= 19; i++) {
+    console.log(i);
+  }
+
