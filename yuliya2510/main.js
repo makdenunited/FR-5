3fdf105d9a4e41947a34f785a41eb3080d93667f
yuliya2510 <goldwing25@mail.ru>
2023-06-27 18:35:42 +0500

    комментарий

---
+++ yuliya2510/main.js
@@ -0,0 +1,15 @@
+let i = 1;
+while (i < 20) {
+  console.log(i);
+  i = i + 1;
+}
+
+let j = 1;
+do {
+  console.log(j);
+  j = j + 1;
+} while (j < 20);
+
+for (let k = 1; k < 20; k++) {
+  console.log(k);
+}
