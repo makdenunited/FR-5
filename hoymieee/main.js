b39e165ad9c993b8ec349eb985914a6ddf32e241
Hoymie <kefirmybro@gmail.com>
2023-06-27 22:31:55 +0200

    da

---
+++ hoymieee/main.js
@@ -0,0 +1,15 @@
+function calc(a, b, operation) {
+    switch (operation) {
+      case "add" :
+        console.log(a + b);
+        break;
+        
+       case "multi" :
+        console.log(a * b);
+        break;
+        
+      case "subtract" :
+        console.log(a - b);
+        break;
+    }
+    }
