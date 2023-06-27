b984139d6706253e96d7412808e1dead3aabdae3
vway1 <one1way55@gmail.com>
2023-06-28 01:40:25 +0300

    Объекты: введение

---
+++ vway1/16/main.js
@@ -0,0 +1,24 @@
+const phoneBook = {
+    list: {
+        "Anna": 111111111,
+        "Ivan Ivanovich": 222222222,
+        "Olga": 333333333,
+        "Petr Petrovich": 444444444,
+        "Svetlana": 555555555
+    },
+    log() {
+        console.log(this.list)
+    }
+};
+
+
+phoneBook.list["Olga"] = 777777777;
+phoneBook.list["Igor Igorevich"] = 666666666;
+phoneBook.list["Anton"] = 888888888;
+delete phoneBook.list["Anna"];
+
+console.log(phoneBook.list.Olga); // 777777777;
+console.log(phoneBook.list["Igor Igorevich"]); // 777777777;
+
+console.log(phoneBook.list) // одинакого с phoneBook.log();
+phoneBook.log();
