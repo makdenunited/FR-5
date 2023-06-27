8a8ef8ebd00b5e9b04e6be8fe372c9f5b0692fce
Arina <ivashkovaarina@gmail.com>
2023-06-27 15:14:50 +0300

    Task 15

---
+++ dartink/main.js
@@ -0,0 +1,17 @@
+let count = 1;
+
+// Первое задание
+// while (count <= 19) {
+//     console.log(count);
+//     count += 1;
+// }
+
+// Второе задание
+// do {
+//     console.log(count++);
+// } while (count <= 19);
+
+// Третье задание
+for (let i = 0; i < 19; i++) {
+    console.log(count++);
+}
