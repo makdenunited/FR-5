86b9d35ece30b6c20306301c811699424286f4cf
Haping61 <tg.trippl3a@gmail.com>
2023-06-27 22:03:06 +0700

    remade the structure, add logContacts method

--- trippl3a/main.js
+++ trippl3a/main.js
@@ -1,36 +1,44 @@
-const contacts = {
-    'name1': {
-        'tag': '@name1',
-        'number': '+7-123-123-1212',
+const phone = {
+    'contactsList': {
+        'name1': {
+            'tag': '@name1',
+            'number': '+7-123-123-1212',
+        },
+        'name2': {
+            'tag': '@name2',
+            'number': '+7-234-234-2323',
+        },
+        'name3': {
+            'tag': '@name3',
+            'number': '+7-345-345-3434',
+        },
     },
-    'name2': {
-        'tag': '@name2',
-        'number': '+7-234-234-2323',
+
+    logContacts() {
+        console.log(this.contactsList)
     },
-    'name3': {
-        'tag': '@name3',
-        'number': '+7-345-345-3434',
-    },
-    'createContact': function(name, number) {
-        this[name] = {
+
+    createContact(name, number) {
+        this.contactsList[name] = {
             'tag': `@${name}`,
             'number': number,
         }
-        console.log(this[name])
+        console.log(this.contactsList[name])
         return;
-    }
+    },
 }
 
-delete contacts.name2
-contacts.name4 = {
+delete phone.contactsList.name2
+phone.contactsList["name4"] = {
     'tag': '@name4',
     'number': '+7-456-456-4545',
 } 
 
-contacts["name1"] = {
+phone.contactsList["name1"] = {
     'tag': '@name12',
     'number': '+7-121-121-1212'
 }
-console.log(contacts)
 
-contacts.createContact('name5', '+7-567-567-5656')
+phone.logContacts()
+phone.createContact('name5', '+7-567-567-5656')
+phone.logContacts()
