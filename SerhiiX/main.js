22990115d1634e86ec1607fcd16b34eee2a3deed
fcprime <75117032+fcprime@users.noreply.github.com>
2023-06-27 09:54:18 +0200

    lesson Object homework

--- SerhiiX/main.js
+++ SerhiiX/main.js
@@ -1,31 +1,22 @@
-let i = 1;
+const phoneBook = {
+  list: {
+    Kent: 987646022,
+    Sniper: 'number009',
+    WalkStreet: 'onlystring',
+    'Maybe Yes': 4487682,
+  },
+  log() {
+    console.log(this.list);
+  },
+};
 
-while (i < 4) {
-  console.log(i);
-  i = i + 1;
-}
+phoneBook.log();
 
-let i = 5;
-do {
-  console.log(i);
-  i = i + 3;
-} while (i < 3);
+phoneBook['Merage Vicente'] = 796094;
+delete phoneBook['Merage Vicente'];
 
-let i = 1;
-do {
-  console.log(i);
-  i = i + 1;
-} while (i <= 19);
-
-for (let i = 1; i < 4; i++) {
-  console.log(i);
-}
-
-for (let i = 1; i <= 19; i++) {
-  console.log(i);
-}
-
-for (let i = 0; i < 10; i++) {
-  if (i % 2 == 1) continue;
-  console.log(i);
-}
+console.log(phoneBook.list.Kent);
+console.log(phoneBook.list.Sniper);
+console.log(phoneBook.list.WalkStreet);
+console.log(phoneBook['Merage Vicente']);
+console.log(phoneBook.list['Maybe Yes']);
