4a949f4b4203ae9152307a36b6412416179e5f95
Alex Sparrow <alexsparrow5973@gmail.com>
2023-06-27 07:36:13 +0300

    objectStart

---
+++ AlexSparrow/dictionary_start.js
@@ -0,0 +1,26 @@
+const itBlogers = {
+    list: {
+        'Стой под стрелой': '@nikitonsky_pub',
+        'ityoutubers.com': '@ityoutubers_com',
+        'Strada': '@stradaone',
+        'Digital Ниндзя': '@digital_ninjaa',
+        'Бобук': '@addmeto'
+    },
+    log() {
+        for (let key in this.list) {
+            console.log(this.list[key]);
+        }
+    }
+};
+
+
+console.log(itBlogers.Strada)
+console.log(itBlogers['Digital Ниндзя'])
+
+itBlogers['Artem Dimitrov'] = '@ArtemDimitrov'
+console.log(itBlogers)
+
+delete itBlogers["ityoutubers.com"]
+console.log(itBlogers)
+
+itBlogers.log()
