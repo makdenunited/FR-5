b5a1a418dd0fb342a94ca572f1d5929343e34eed
Ruslan Rezyapov <rruslan97@yandex.ru>
2023-06-28 15:11:58 +0500

    practice17

---
+++ rruslan97-17/main.js
@@ -0,0 +1,29 @@
+const phoneBook = {
+    list: {
+        "Ruslan": 5,
+        "Diana": 26,
+        "Mama Roza": 9,
+        "Papa Yurik": 10,
+        "Bonya": 12
+    },
+  add (name, number) {
+    this.list[name] = number;
+  },
+  remove (qwe) {
+    delete phoneBook.list[qwe];
+  }
+};
+
+// console.log(phoneBook.list);
+// phoneBook.add ('Jack', 1234567);
+// console.log(phoneBook.list);
+// phoneBook.remove ("Bonya");
+// console.log(phoneBook.list);
+
+
+
+
+for (const name in phoneBook.list) {
+    console.log(name + '-'+ phoneBook.list[name]);
+    // console.log(phoneBook.list[name] );
+  }
