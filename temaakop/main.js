8b8f712befcb3fe5f51d7e93d1f526f35e946813
temaakop <akopov_ad@mail.ru>
2023-06-28 12:01:04 +0300

    practice-17

---
+++ temaakop/main.js
@@ -0,0 +1,25 @@
+const phoneBook = {
+    list: {
+      "John": 123456789,
+      "Jane Doe": 987654321,
+      "Jim Smith": 111111111
+    },
+    add (name, number) {
+      this.list[name] = number
+    },
+    
+    delete (name) {
+      delete this.list[name]
+    }
+  };
+  
+  phoneBook.add('Artem Akopov', 7)
+  phoneBook.delete('Jane Doe')
+  console.log(phoneBook.list)
+  console.log('John' in phoneBook.list)
+  console.log(phoneBook.list.John)
+  
+  
+  for (let name in phoneBook.list) {
+    console.log(name)
+  }
