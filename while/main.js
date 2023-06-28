850bf2cf6945f93b3c88a1b1ddea1a715157f1ce
enakinena <jenyakuraev3@gmail.com>
2023-06-28 12:22:35 +0300

    first commit

---
+++ while/main.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 18; i++) {
+  if (i % 2 == 1) {
+    console.log(`${i} - нечётное число `);
+  } else {
+    console.log(`${i} - чётное число`);
+  }
+}
+
+for (let i = 1; i <= 18; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} - чётное число`);
+  }
+}
+
+for (let i = 1; i <= 18; i++) {
+  if (i % 2 == 1) {
+    console.log(`${i} - нечётное число`);
+  }
+}
