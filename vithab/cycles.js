ed82168baaf9aed82eecdd8a6ebbe99341d873dd
Vitaly <vit.bogachev@gmail.com>
2023-06-28 06:22:27 +0300

    Cycles

---
+++ vithab/cycles.js
@@ -0,0 +1,23 @@
+let i = 1;
+
+while (i <= 19) {
+  console.log(i);
+  i++;
+}
+
+console.log('===================')
+
+i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 19);
+
+console.log('===================')
+
+i = 1;
+
+for (; i <= 19; i++) {
+  console.log(i);
+}
